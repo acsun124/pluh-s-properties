136516a865757d863e66016e28ce19af620f18aa
acsun124 <138546987+acsun124@users.noreply.github.com>
2024-06-26 23:39:31 +0800

    Update test.lua
    bringed it back again cus im dumb

--- test.lua
+++ test.lua
@@ -1,14 +1,16 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
-local Mouse = game.Players.LocalPlayer:GetMouse()
-local CamlockState = false
-local Prediction = 0.1687963
-local HorizontalPrediction = 0.1760773
-local VerticalPrediction = 0.160692
-local XPrediction = 0.1760773
-local YPrediction = 0.160692
 
+getgenv().Prediction = 0.1248710929171	
+getgenv().AimPart = "HumanoidRootPart"	
+getgenv().Key = "Q"	
+getgenv().DisableKey = "P"	
+	
+getgenv().FOV = true	
+getgenv().ShowFOV = false	
+getgenv().FOVSize = 55	
+	
+--// Variables (Service)	
+	
+local Players = game:GetService("Players")	
 local RS = game:GetService("RunService")	
 local WS = game:GetService("Workspace")	
 local GS = game:GetService("GuiService")	
@@ -23,12 +25,65 @@ local GetGuiInset = GS.GetGuiInset
 	
 local AimlockState = true	
 local Locked	
-local enemy
-
-
+local Victim	
+	
+local SelectedKey = getgenv().Key	
+local SelectedDisableKey = getgenv().DisableKey	
+	
+--// Notification function	
+	
+function Notify(tx)	
+    SG:SetCore("SendNotification", {	
+        Title = "Pluh / Tief's CamLock",	
+        Text = tx,	
+Duration = 5	
+    })	
+end	
+	
+--// Check if aimlock is loaded	
+	
+if getgenv().Loaded == true then	
+    Notify("Aimlock is already loaded!")	
+    return	
+end	
+	
+getgenv().Loaded = true	
+	
+--// FOV Circle	
+	
+local fov = Drawing.new("Circle")	
+fov.Filled = false	
+fov.Transparency = 1	
+fov.Thickness = 1	
+fov.Color = Color3.fromRGB(255, 255, 0)	
+fov.NumSides = 1000	
+	
+--// Functions	
+	
+function update()	
+    if getgenv().FOV == true then	
+        if fov then	
+fov.Radius = getgenv().FOVSize * 2	
+            fov.Visible = getgenv().ShowFOV	
+fov.Position = Vector2.new(Mouse.X, Mouse.Y + GetGuiInset(GS).Y)	
+	
+            return fov	
+        end	
+    end	
+end	
+	
+function WTVP(arg)	
+    return Camera:WorldToViewportPoint(arg)	
+end	
+	
+function WTSP(arg)	
+    return Camera.WorldToScreenPoint(Camera, arg)	
+end	
+	
 function FindNearestEnemy()
     local ClosestDistance, ClosestPlayer = math.huge, nil
-    local CenterPosition = Vector2.new(
+    local CenterPosition =
+        Vector2.new(
         game:GetService("GuiService"):GetScreenResolution().X / 2,
         game:GetService("GuiService"):GetScreenResolution().Y / 2
     )
@@ -36,8 +91,9 @@ function FindNearestEnemy()
     for _, Player in ipairs(game:GetService("Players"):GetPlayers()) do
         if Player ~= LocalPlayer then
             local Character = Player.Character
-            if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChildOfClass("Humanoid") and Character.Humanoid.Health > 0 then
-                local Position, IsVisibleOnViewport = game:GetService("Workspace").CurrentCamera:WorldToViewportPoint(Character.HumanoidRootPart.Position)
+            if Character and Character:FindFirstChild("HumanoidRootPart") and Character.Humanoid.Health > 0 then
+                local Position, IsVisibleOnViewport =
+                    game:GetService("Workspace").CurrentCamera:WorldToViewportPoint(Character.HumanoidRootPart.Position)
 
                 if IsVisibleOnViewport then
                     local Distance = (CenterPosition - Vector2.new(Position.X, Position.Y)).Magnitude
@@ -55,7 +111,34 @@ end
 
 local enemy = nil
 
--- Function to aim the camera at the nearest enemy's HumanoidRootPart
+Mouse.KeyDown:Connect(function(k)	
+    SelectedKey = SelectedKey:lower()	
+    SelectedDisableKey = SelectedDisableKey:lower()	
+    if k == SelectedKey then	
+        if AimlockState == true then	
+            Locked = not Locked	
+            if Locked then	
+                enemy = FindNearestEnemy()	
+	
+                Notify("Locked onto: "..tostring(enemy.Character.Humanoid.DisplayName))	
+            else	
+                if enemy ~= nil then	
+                    enemy = nil	
+	
+                    Notify("Unlocked!")	
+                end	
+            end	
+        else	
+            Notify("Aimlock is not enabled!")	
+        end	
+    end	
+    if k == SelectedDisableKey then	
+        AimlockState = not AimlockState	
+    end	
+end)	
+	
+--// Loop update FOV and loop camera lock onto target	
+	
 RS.RenderStepped:Connect(function()	
     update()	
     if AimlockState == true then	
@@ -65,60 +148,79 @@ RS.RenderStepped:Connect(function()
     end	
 end)	
 
--- Create GUI
-local Tief = Instance.new("ScreenGui")
+local Hellbound = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
+local Logo = Instance.new("ImageLabel")
 local TextButton = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
 
--- Properties:
+--Properties:
 
-Tief.Name = "Tief"
-Tief.Parent = game.CoreGui
-Tief.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Hellbound.Name = "Hellbound"
+Hellbound.Parent = game.CoreGui
+Hellbound.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Frame.Parent = Tief
+Frame.Parent = Hellbound
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.5, -101, 0.5, -35) -- Center the frame
+Frame.Position = UDim2.new(0.133798108, 0, 0.20107238, 0)
 Frame.Size = UDim2.new(0, 202, 0, 70)
 Frame.Active = true
 Frame.Draggable = true
 
+local function TopContainer()
+	Frame.Position = UDim2.new(0.5, -Frame.AbsoluteSize.X / 2, 0, -Frame.AbsoluteSize.Y / 2)
+end
+
+TopContainer()
+Frame:GetPropertyChangedSignal("AbsoluteSize"):Connect(TopContainer)
+
 UICorner.Parent = Frame
 
+Logo.Name = "Logo"
+Logo.Parent = Frame
+Logo.BackgroundColor3 = Color3.fromRGB(100, 255, 100)
+Logo.BackgroundTransparency = 3.000
+Logo.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Logo.BorderSizePixel = 0
+Logo.Position = UDim2.new(0.326732665, 0, 0, 0)
+Logo.Size = UDim2.new(0, 70, 0, 70)
+Logo.Image = "rbxassetid://830610397"
+Logo.ImageTransparency = 0.300
+
 TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0) -- Full black background
-TextButton.BackgroundTransparency = 0.0 -- No transparency
-TextButton.BorderColor3 = Color3.fromRGB(255, 255, 255) -- Border color white
+TextButton.BackgroundColor3 = Color3.fromRGB(75, 80, 255)
+TextButton.BackgroundTransparency = 5.000
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0.1, 0, 0.1, 0)
+TextButton.Position = UDim2.new(0.0792079195, 0, 0.18571429, 0)
 TextButton.Size = UDim2.new(0, 170, 0, 44)
 TextButton.Font = Enum.Font.SourceSansSemibold
-TextButton.Text = "TIEF"
-TextButton.TextColor3 = Color3.fromRGB(255, 255, 255) -- Text color white
+TextButton.Text = "Toggle Hellbound"
+TextButton.TextColor3 = Color3.fromRGB(180, 180, 255)
 TextButton.TextScaled = true
-TextButton.TextSize = 14.000
+TextButton.TextSize = 18.000
 TextButton.TextWrapped = true
-
 local state = true
-TextButton.MouseButton1Click:Connect(function()
-    state = not state
-    if state then
-        TextButton.Text = "T LOCK"
-        AimlockState = false
-        enemy = nil 
-    else
-        TextButton.Text = "T UNLOCK"
-        AimlockState = true
-        enemy = FindNearestEnemy()
+TextButton.MouseButton1Click:Connect(
+    function()
+        state = not state
+        if not state then
+            TextButton.Text = "Hellbound ON"
+            AimlockState = true
+            enemy = FindNearestEnemy()
+            
+        else
+            TextButton.Text = "Hellbound OFF"
+            enemy = nil
+            AimlockState = false
+        end
     end
-end)
-UICorner_2.Parent = TextButton
+)
 
-while wait() do
+	while wait() do
         if getgenv().AutoPrediction == true then	
         local pingvalue = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()	
         local split = string.split(pingvalue,'(')	
@@ -167,4 +269,5 @@ elseif ping < 20 then
 getgenv().Prediction = 0.157	
         end	
         end	
-end
+	end
+  
